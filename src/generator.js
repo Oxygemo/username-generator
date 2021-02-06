a1a2f1aa662568d149286e5c38e2ff90c01cf5c8
Oxygemo <alej0hio2007@gmail.com>
2021-02-06 14:53:43 +0100

    fix(generator_settings): add a checkbox (tbf)
    Signed-off-by: Oxygemo <alej0hio2007@gmail.com>

--- src/generator.js
+++ src/generator.js
@@ -6,7 +6,24 @@ Array.prototype.random = function () {
 
 console.log("It's alive!")
 
+function siteLoad() {
+    changeFooterText();
+}; 
+
 function changeFooterText() {
     const textlist = ["Made for fun by Odyssey346", "Made with love by Odyssey346", "Made to mess around with JS by Odyssey346"];
     document.getElementById('footertext').innerHTML = textlist.random()
 };
+
+
+function Checkbox1Clicked() {
+    if (document.querySelector('#appendTheCheckbox:checked') !== null) {
+        var Checkbox1Checked = true;
+        console.log("checkbox is ticked.")
+    }
+    else {
+        var Checkbox1Checked = false;
+        console.log("checkbox isn't ticked.")
+    }
+    console.log(Checkbox1Checked);
+}
