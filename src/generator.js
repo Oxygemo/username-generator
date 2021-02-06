c615acbe7216f83bbbd5c8a81b1e13e7f792c9b3
Oxygemo <alej0hio2007@gmail.com>
2021-02-06 13:17:23 +0100

    fix(changeFooterText): optimize it
    Signed-off-by: Oxygemo <alej0hio2007@gmail.com>

--- src/generator.js
+++ src/generator.js
@@ -1,13 +1,10 @@
+/* If you plan on using this code, or a part of it, you need to give credit. */
+
 Array.prototype.random = function () {
     return this[Math.floor((Math.random()*this.length))];
 };
 
-window.onload = function() {
-    changeFooterText();
-};
-
 console.log("It's alive!")
-
 
 function changeFooterText() {
     const textlist = ["Made for fun by Odyssey346", "Made with love by Odyssey346", "Made to mess around with JS by Odyssey346"];
