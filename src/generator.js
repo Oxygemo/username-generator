f66b5b3c244b9d423be0721f2bef65f5443965f2
Oxygemo <alej0hio2007@gmail.com>
2021-02-06 13:08:39 +0100

    feat(footer): add a footer and link to source code
    Signed-off-by: Oxygemo <alej0hio2007@gmail.com>

--- src/generator.js
+++ src/generator.js
@@ -1,1 +1,15 @@
+Array.prototype.random = function () {
+    return this[Math.floor((Math.random()*this.length))];
+};
+
+window.onload = function() {
+    changeFooterText();
+};
+
 console.log("It's alive!")
+
+
+function changeFooterText() {
+    const textlist = ["Made for fun by Odyssey346", "Made with love by Odyssey346", "Made to mess around with JS by Odyssey346"];
+    document.getElementById('footertext').innerHTML = textlist.random()
+};
